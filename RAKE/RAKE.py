7e5b03287907ffdfe55ab83eddfba53c367890c2
Fabian von Feilitzsch <fabian@fabianism.us>
2016-09-15 16:08:46 -0400

    Flake8 compliant style

--- RAKE/RAKE.py
+++ RAKE/RAKE.py
@@ -1,11 +1,12 @@
 # Implementation of RAKE - Rapid Automtic Keyword Exraction algorithm
 # as described in:
-# Rose, S., D. Engel, N. Cramer, and W. Cowley (2010). 
-# Automatic keyword extraction from indi-vidual documents. 
+# Rose, S., D. Engel, N. Cramer, and W. Cowley (2010).
+# Automatic keyword extraction from indi-vidual documents.
 # In M. W. Berry and J. Kogan (Eds.), Text Mining: Applications and Theory.unknown: John Wiley and Sons, Ltd.
 
 import re
 import operator
+
 
 def is_number(s):
     try:
@@ -39,7 +40,7 @@ def separate_words(text, min_word_return_size):
     words = []
     for single_word in splitter.split(text):
         current_word = single_word.strip().lower()
-        #leave numbers in phrase, but don't count as words, since they tend to invalidate scores of their phrases
+        # leave numbers in phrase, but don't count as words, since they tend to invalidate scores of their phrases
         if len(current_word) > min_word_return_size and current_word != '' and not is_number(current_word):
             words.append(current_word)
     return words
@@ -59,7 +60,7 @@ def build_stop_word_regex(stop_word_file_path):
     stop_word_list = load_stop_words(stop_word_file_path)
     stop_word_regex_list = []
     for word in stop_word_list:
-        word_regex = r'\b' + word + r'(?![\w-])'  # added look ahead for hyphen
+        word_regex = r'\b' + word + r'(?![\w-])'
         stop_word_regex_list.append(word_regex)
     stop_word_pattern = re.compile('|'.join(stop_word_regex_list), re.IGNORECASE)
     return stop_word_pattern
@@ -84,13 +85,11 @@ def calculate_word_scores(phraseList):
         word_list = separate_words(phrase, 0)
         word_list_length = len(word_list)
         word_list_degree = word_list_length - 1
-        #if word_list_degree > 3: word_list_degree = 3 #exp.
         for word in word_list:
             word_frequency.setdefault(word, 0)
             word_frequency[word] += 1
             word_degree.setdefault(word, 0)
-            word_degree[word] += word_list_degree  #orig.
-            #word_degree[word] += 1/(word_list_length*1.0) #exp.
+            word_degree[word] += word_list_degree
     for item in word_frequency:
         word_degree[item] = word_degree[item] + word_frequency[item]
 
@@ -98,8 +97,7 @@ def calculate_word_scores(phraseList):
     word_score = {}
     for item in word_frequency:
         word_score.setdefault(item, 0)
-        word_score[item] = word_degree[item] / (word_frequency[item] * 1.0)  #orig.
-    #word_score[item] = word_frequency[item]/(word_degree[item] * 1.0) #exp.
+        word_score[item] = word_degree[item] / (word_frequency[item] * 1.0)
     return word_score
 
 
--- RAKE/__init__.py
+++ RAKE/__init__.py
@@ -1,4 +1,4 @@
 try:
-    from RAKE import Rake
+    from RAKE import Rake  # NOQA
 except ImportError:
-    from RAKE.RAKE import Rake
+    from RAKE.RAKE import Rake  # NOQA
