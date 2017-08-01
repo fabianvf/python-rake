e96faca352cd6ff3d1a704913e32a79c88ddf190
Justin Terry <justinkterry@gmail.com>
2017-08-01 17:18:25 -0400

    fixes divide support

--- RAKE/RAKE.py
+++ RAKE/RAKE.py
@@ -50,23 +50,18 @@ def load_stop_words(stop_word_file, divide):
                 stop_words.append(word)
     return stop_words
 
-def load_stop_words_delimiter(stop_word_file, delimiter, divide):
+def load_stop_words_delimiter(stop_word_file, delimiter):
     """
     Utility function to load stop words from a file and return as a list of words
     @param stop_word_file Path and file name of a file containing stop words.
     @return list A list of stop words.
     """
     stop_words = []
-    if divide:
-        for line in open(stop_word_file):
-            for word in line.split(delimiter):  # in case more than one per line
-                #handles .csvs or a comma seperated list, or equivalent. 
-                #'asd,'.split(',') returns ['asd', ''] but 'asd '.split() returns 'asd'
-                if word !='': 
-                    stop_words.append(word)
-    else:
-        for line in open(stop_word_file):
-            for word in line.split(delimiter[0]): #motivated as explained right above
+    for line in open(stop_word_file):
+        for word in line.split(delimiter):  # in case more than one per line
+            #handles .csvs or a comma seperated list, or equivalent. 
+            #'asd,'.split(',') returns ['asd', ''] but 'asd '.split() returns 'asd'
+            if word !='': 
                 stop_words.append(word)
     return stop_words
 
@@ -159,10 +154,13 @@ class Rake(object):
         if isinstance(stop_words, list):
             self.__stop_words_pattern = build_stop_word_regex(stop_words)
         else:
-            if delimiter == ' ':
-                self.__stop_words_pattern = build_stop_word_regex(load_stop_words(stop_words,divide))  # handles normal file paths
-            else:
-                self.__stop_words_pattern = build_stop_word_regex(load_stop_words_delimiter(stop_words,divide,delimiter))
+            if delimiter != ' ' && divide == False:
+                #send error and stop here
+                else:
+                    if divide == True:
+                        self.__stop_words_pattern = build_stop_word_regex(load_stop_words(stop_words,divide))
+                    else:
+                        self.__stop_words_pattern = build_stop_word_regex(load_stop_words_delimiter(stop_words,delimiter))
 
     def run(self, text):
         sentence_list = split_sentences(text)
