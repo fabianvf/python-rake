690380fd72aa8371f5fe4b3c6c34bd4a37c5467b
Justin Terry <justinkterry@gmail.com>
2017-08-08 05:21:59 -0400

    Create GoogleSearchStopList.py

---
+++ RAKE/stoplists/GoogleSearchStopList.py
@@ -0,0 +1,43 @@
+#from http://www.ranks.nl/stopwords , copy of permission granting here https://github.com/fabianvf/python-rake/issues/20
+
+#based on suspected stopwords from analyzing displayed google search suggestions before they had to remove them over
+#people making lists like this (or "why we can't have nice things)
+
+wordlist = [
+"a",
+"about",
+"an",
+"are",
+"as",
+"at",
+"be",
+"by",
+"com",
+"for",
+"from",
+"how",
+"i",
+"in",
+"is",
+"it",
+"of",
+"on",
+"or",
+"that",
+"the",
+"the",
+"this",
+"to",
+"was",
+"what",
+"when",
+"where",
+"who",
+"will",
+"with",
+"www"
+]
+
+
+def words():
+    return wordlist
