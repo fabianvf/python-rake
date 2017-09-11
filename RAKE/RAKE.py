1c9ce278b80c1d2c78afe86562e79b742052cd5a
Justin Terry <justinkterry@gmail.com>
2017-09-11 19:19:06 -0400

    Update RAKE.py

--- RAKE/RAKE.py
+++ RAKE/RAKE.py
@@ -79,7 +79,7 @@ def split_sentences(text):
     Utility function to return a list of sentences.
     @param text The text that must be split in to sentences.
     """
-    sentence_delimiters = re.compile(u'[.!?,;:\t\\\\"\\(\\)\\\'\u2019\u2013]|\\s\\-\\s')
+    sentence_delimiters = re.compile(u'[.!?,;:\t\\\\"\\(\\)\\\'\u2019\u2013]|\\s\\-\\s') #upgrade as appropriate, fabian had changed version?
     sentences = sentence_delimiters.split(text)
     return sentences
 
