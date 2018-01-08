7ad3e55d50f7a984414cd0b1ae0c116880fc023d
Justin Terry <justinkterry@gmail.com>
2018-01-08 15:08:56 -0500

    works with new stop lists in a way that works, includes new stop lists

--- RAKE/__init__.py
+++ RAKE/__init__.py
@@ -1,4 +1,8 @@
-try:
-    from RAKE import Rake, SmartStopList, FoxStopList, MySQLStopList, NLTKStopList # NOQA
-except ImportError:
-    from RAKE.RAKE import Rake, SmartStopList, FoxStopList, MySQLStopList, NLTKStopList # NOQA
+from RAKE import RAKE
+from stoplists import FoxStopList
+from stoplists import GoogleSearchStopList
+from stoplists import MySQLStopList
+from stoplists import NLTKStopList
+from stoplists import RanksNLLongStopList
+from stoplists import RanksNLStoplist
+from stoplists import SmartStopList
