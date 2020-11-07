66e91b7ccb6f563367d7ad07a8d632cd01a7265e
Justin Terry <justinkterry@gmail.com>
2020-11-06 22:02:28 -0500

    fix classifiers

--- setup.py
+++ setup.py
@@ -47,6 +47,7 @@ setup(
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
